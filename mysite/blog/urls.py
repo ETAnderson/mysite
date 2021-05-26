ec900f27fb6ae04d8103bcaa99e20c6431998e87
Eric Anderson <eric@pop-os.localdomain>
2021-05-26 14:50:36 -0400

    comments not WAI

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
-    path('', views.index, name="homepage"),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from .forms import CommentForm
 from .models import Post
 # Create your views here.
 
@@ -11,7 +12,26 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
 
+def post_detail(request, slug):
+    template_name = 'post_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True).order_by("-created_on")
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
 
-def index(request):
-    my_dict = {"insert_me": "I am from views.py"}
-    return render(request,'index.html',context=my_dict)
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
