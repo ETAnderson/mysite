3a25f678663900ce5625c60f34e691c7913c6066
Eric Anderson <eric@pop-os.localdomain>
2021-05-26 15:29:44 -0400

    commenting now wai

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -14,9 +14,8 @@ class CommentAdmin(admin.ModelAdmin):
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'status', 'created_on')
-    list_filter = ("status",)
+    list_filter = ("status", "created_on")
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Post, PostAdmin)
-# Register your models here.
