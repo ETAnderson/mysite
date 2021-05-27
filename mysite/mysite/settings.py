7a11f58ba8a3a6262920f68e9ad08f7bcef3863e
Eric Anderson <eric@pop-os.localdomain>
2021-05-27 13:02:26 -0400

    initiated static styling and beginning customization

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Location of static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
