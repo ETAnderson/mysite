228d186def8709220db8cdc72492b77c10692544
Eric Anderson <anderson.eric.t@gmail.com>
2021-06-09 17:22:49 -0400

    changed path in wsgi.py to mysite from mysite.settings

--- mysite/mysite/wsgi.py
+++ mysite/mysite/wsgi.py
@@ -14,7 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mysite")
 
 application = get_wsgi_application()
