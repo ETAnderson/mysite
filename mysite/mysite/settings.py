2d40c58ad25bcc8cb4520b89f7db7a13d9ba94f7
Eric Anderson <anderson.eric.t@gmail.com>
2021-06-09 15:59:05 -0400

    the wondrous workings of deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,5 +1,3 @@
-import django_heroku
-
 """
 Django settings for mysite project.
 
@@ -11,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
