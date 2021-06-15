d2da5b762a2be91f475888a94899bd1c5256ab40
Eric Anderson <anderson.eric.t@gmail.com>
2021-06-15 14:09:11 -0400

    reverting urls.py for blogs due to no home app in place TBC

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('blogs/', views.PostList.as_view(), name='home'),
+    path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='blog_post_detail'),
 ]
