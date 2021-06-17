5453e6a52c36f83caf8a085c27794943ad30bbc8
Eric Anderson <anderson.eric.t@gmail.com>
2021-06-17 15:22:00 -0400

    added home to settings app list, set up base view function, added home to urls.py, initialized home template and extracted mission statement from blog page to home page

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'polls.apps.PollsConfig',
     'blog',
+    'home,'
 ]
 
 MIDDLEWARE = [
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from home.views import HomePageView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', HomePageView.as_view(), name='home'),
+    path('blogs/', include('blog.urls')),
     path('polls/', include('polls.urls')),
 ]
