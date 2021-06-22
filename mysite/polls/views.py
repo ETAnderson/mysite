bd3a1cf357cb57b49207ea12136f60b9dc586d93
Eric Anderson <anderson.eric.t@gmail.com>
2021-06-22 15:32:10 -0400

    moved submit button back into form, not sure why I ever moved it out

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 from .models import Choice, Question
 
 class IndexView(generic.ListView):
-    template_name = 'index.html'
+    template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
